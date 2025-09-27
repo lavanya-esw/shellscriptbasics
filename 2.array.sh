97a0b1d21d6a932a2e5c3f5c4c80dfbe1c026c2d
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-27 11:41:37 +0530

    about date command

--- 2.array.sh
+++ 2.array.sh
@@ -4,3 +4,7 @@ FRUITS=("apple" "banana" "orange" "pineappale")
 
 LENGTH=${#FRUITS[@]}
 echo "Lenth of the array is $LENGTH"
+echo "first element : ${FRUITS[0]}"
+echo "second element : ${FRUITS[1]}"
+echo "third element : ${FRUITS[2]}"
+echo "fourth element : ${FRUITS[3]}"
