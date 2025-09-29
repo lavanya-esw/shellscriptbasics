2283407f626d53d1e9a3dc56a2f76189f04787fd
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-29 16:57:59 +0530

    filtering the lines

--- Filter_lines.sh
+++ Filter_lines.sh
@@ -7,7 +7,7 @@ FILE=$FILEPATH/data.csv
 
 
 while IFS= read -r line; do
-    if echo "$line" | grep -q "Devops"; then
+    if echo "$line" | grep -iq "Devops"; then
         echo "$line" >> "output.log"
     fi
 
