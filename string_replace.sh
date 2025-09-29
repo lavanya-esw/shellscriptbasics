391a04a8cf3a5f4297860609c94704fa6358c4cb
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-29 16:28:10 +0530

    string replacement

--- string_replace.sh
+++ string_replace.sh
@@ -5,7 +5,7 @@ if [ ! -z path ]; then
    
    Files=$(find . -iname "*.txt")
    echo -e "\n files are:"
-   echo $Files
+   echo -e "\n $Files"
    for file in $FILE; do
    sed -i 's/FOO/BAR/g'
    done 
