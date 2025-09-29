2f0ce2fe871a4a3213a4b49be28316dd2ee20696
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-29 16:46:28 +0530

    filtering the lines

--- Filter_lines.sh
+++ Filter_lines.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 FILEPATH=$PWD
+FILE=$FILEPATH/data.csv
+output_file=$FILEPATH/output.log
+
+mkdir -p $output_file
+
+while IFS=read -r line; do
+
+echo "grep -q ERROR $line 1>> $output_file"
+
+done < "$FILE"
