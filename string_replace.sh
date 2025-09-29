28e3ac13ba533681e24575ee37377ca89b7b5b41
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-29 16:30:15 +0530

    string replacement

--- string_replace.sh
+++ string_replace.sh
@@ -5,7 +5,7 @@ if [ ! -z path ]; then
    
    for file in $(find . -iname "*.txt"); do
    echo "file"$file"
-   sed -i 's/FOO/BAR/g'
+   sed -i 's/FOO/BAR/g' $file   
    echo "string replaced"
    done 
 fi
