e954dc3b38afb55bc9ea3256cf0dfa30dfdcc32a
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-29 16:04:11 +0530

    worls count

--- word_count.sh
+++ word_count.sh
@@ -8,8 +8,8 @@ if [ ! -z $PATH ]; then
         echo "given $PATH is a file"
         if [ -s $PATH ]; then 
             echo "given file is not empty"
-            echo "Total lines:"$(wc -l $PATH)""
-            echo "Total words:$(wc -w $PATH)"
+            echo "Total lines:"$(wc -l < $PATH)""
+            echo "Total words:$(wc -w < $PATH)"
             echo "Total characters:$(wc -m $PATH)"
         fi
         if [ -r $PATH ] && [ -w $PATH ]; then
