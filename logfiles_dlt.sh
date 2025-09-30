71a8107f9e4eece57bf13027386d31a013673786
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-30 10:39:46 +0530

    delete log files

--- logfiles_dlt.sh
+++ logfiles_dlt.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-FILES=$(find -type f -iname "*.log" -mtime +14)
 
-echo -e "\n $FILES"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
 
-while IFS= read -r file; do
+SOURCE_DIR=$1
 
-echo "deleting the log file"
+echo -e "$B script strted at $(date +%F-%T) $N"
 
-echo "$(rm -rf $file)$file....deleted"
-done <<< "$FILES"
+if [ ! -z $SOURCE_DIR ]; then
+    echo "source $SOURCE_DIR is exists"
+    FILES=$(find -type f -iname "*.log" -mtime +14)
+    if [ $? eq 0 ]; then 
+        echo -e "\n $FILES"
+        while IFS= read -r file; do
+            echo -e "$(rm -rf $file)$file....$R deleted $N"
+        done <<< "$FILES"
+    fi
+else
+    echo -e " $R source $SOURCE_DIR is not found $N"
+fi
