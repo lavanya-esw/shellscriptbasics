4f5c863eefe9f199e2d0a54c12b64af776c6ff93
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-09-30 11:49:18 +0530

    diskusage

---
+++ Monitoring/disk_usage_alert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+THERSHOLD="2"
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+
+
+
+while IFS= read -r line; do
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7}')
+    if [  $USAGE -ge $THERSHOLD ]; then
+        MESSAGE+="High usage at $PARTITION:$UAGAE% \n"
+    fi
+done <<< "$DISK_USAGE"
+
+echo -e "${MESSAGE}"
