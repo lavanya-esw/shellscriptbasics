062c7f338d73acdfc1394bdc0fc4c0868a2277b9
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-03 15:29:31 +0530

    cpu usage

--- Monitoring/cpu_usage.sh
+++ Monitoring/cpu_usage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
 echo "IDLE IS $CPU_IDLE"
@@ -6,7 +7,7 @@ CPU_USAGE=$(echo "100 - $CPU_IDLE" | bc )
 echo " cpu usage is $CPU_USGAE"  
 CPU_USAGE_INT=$(printf "%.0f" "$CPU_USAGE")
 
-THERSHOLD=3
-if [ "$CPU_USAGE_INT" -ge "$THERSHOLD" ]; then
+THERSHOLD="3"
+if [ "${CPU_USAGE_INT}" -ge "${THERSHOLD}" ]; then
     echo "ALERT......CPU USGAE  IS ${CPU_USGAE_INT}%"
 fi
