eae67db75a16ce38defbd1ea7d5b88c06a33834f
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-03 16:23:56 +0530

    pyramid_stars

--- pyramid_stars.sh
+++ pyramid_stars.sh
@@ -3,6 +3,9 @@ read n
 for (( i=1; i<=n; i++ )); do
 #for (( i=0; i<5; i++ )); do
     for((j=i; j<=n; j++)); do
+        echo -n ""
+    done
+    for ((j=1; j<2*i-1; j++)); do
         echo -n "*"
     done
     
