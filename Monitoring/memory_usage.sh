0ae4a0d6cd32d100c2a87d74f75a67a0ab161bfc
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-03 15:07:10 +0530

    memory usage

--- Monitoring/memory_usage.sh
+++ Monitoring/memory_usage.sh
@@ -4,7 +4,7 @@ USED_MEM=$(free -m | awk '/Mem:/ {print $3}')
 TOTAL_MEMORY=$(free -m | awk '/Mem:/ {print $2}')
 USGAE_IN_PERCENTAGE=$((($USED_MEM  * 100)/$TOTAL_MEMORY))
 
-THERSHOLD=30
+THERSHOLD=10
 
 
 echo "memory usage is ${USGAE_IN_PERCENTAGE}%"
