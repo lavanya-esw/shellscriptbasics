2fbf58e76b83215b39f546597e906e8e5be7a088
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-03 15:24:23 +0530

    cpu usage

--- Monitoring/cpu_usage.sh
+++ Monitoring/cpu_usage.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
+echo "IDLE IS $CPU_IDLE"
 CPU_USAGE=$(echo "100 - $CPU_IDLE" | bc )
+echo " cpu usage is $CPU_USGAE"  
 CPU_USAGE_INT=$(printf "%.0f" "$CPU_USAGE")
 
 echo "cpu usage is $CPU_USAGE_INT"
