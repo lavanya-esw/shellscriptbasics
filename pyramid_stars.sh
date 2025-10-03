2b1185cdad14a30722cf437249896c56521ed0d7
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-03 16:31:24 +0530

    pyramid_stars

--- pyramid_stars.sh
+++ pyramid_stars.sh
@@ -19,7 +19,7 @@ read -p "Enter number of rows: " n
 
 for((i=1; i<=n; i++)); do
     for((j=i; j<n; j++)); do
-        echo -n ""
+        echo -n " "
     done
 
     for(( j=1; j<=(2*i-1); j++ )); do
