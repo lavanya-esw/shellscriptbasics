d78b090b36110a0bffbea701cddeedda507932c2
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-10-26 21:25:32 +0530

    file extension change

---
+++ file_extension_change.sh
@@ -0,0 +1,24 @@
+#!/bin/bash/
+
+## changing file extension .txt to .html
+
+if [ $# -ne 1 ]; then
+    echo "usage : script.sh <path_of_directort>"
+    echo "Example: script.sh "path/to/myfiles"
+    exit 1
+fi
+
+DIR_PATH = $1
+
+if [ ! -d $DIR_PATH ]; then
+    echo "Error: Directory '$DIR_PATH' not found."
+    exit 1
+fi
+
+cd $DIR_PATH
+
+for file in *.txt; do
+    mv $file ${file%.txt}.html
+done
+
+echo "File extension change complete..."
